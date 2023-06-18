349483d769e3d7b1cac3b7c38204e630ba158035
pralim <limbuprajil123@gmail.com>
2023-06-19 02:00:25 +0300

    Edit button fix

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,71 +1,109 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react';
 import { useTable } from 'react-table';
-// import TodoEach from './TodoEach'
 
-export default function TodoList({lists, toggleTodo, editTodo}) {
+export default function TodoList({ lists, toggleTodo, editTodo }) {
+  const [editingTodoId, setEditingTodoId] = useState(null);
+  const [editValue, setEditValue] = useState(lists.name);
 
-    const data = React.useMemo(() => lists, [lists]);
-    const columns = React.useMemo(
-        () => [
-        {
-            Header: 'Todo',
-            accessor: 'name',
+  const handleEditInputChange = (event) => {
+    setEditValue(event.target.value);
+  };
+
+  const handleEditSave = useCallback(
+    (todoId) => {
+        editTodo(todoId, editValue);
+        setEditingTodoId(null);
+        setEditValue('');
         },
-        {
-            Header: 'Completed',
-            accessor: 'complete',
-            Cell: ({ value }) => <input type="checkbox" checked={value} readOnly />,
-        },
-        {
-            Header: 'Edit',
-            accessor: 'id',
-            Cell: ({ value }) => <button onClick={() => editTodo(value)}>Edit</button>,
-        },
-        ],
-        [editTodo]
-    );
+        [editTodo, editValue]
+  );
 
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-    } = useTable({ columns, data });
-
-    return (
-        <table {...getTableProps()}>
-        <thead>
-            {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-                ))}
-            </tr>
-            ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-            {rows.map((row) => {
-            prepareRow(row);
+  const data = React.useMemo(() => lists, [lists]);
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Todo',
+        accessor: 'name',
+        Cell: ({ row }) => {
+          if (row.original.id === editingTodoId) {
             return (
-                <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => (
-                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                ))}
-                </tr>
+              <div>
+                <input
+                  type="text"
+                  value={editValue}
+                  onChange={handleEditInputChange}
+                />
+                <button onClick={() => handleEditSave(row.original.id)}>
+                  Save
+                </button>
+              </div>
             );
-            })}
-        </tbody>
-        </table>
-    );
+          } else {
+            return row.original.name;
+          }
+        },
+      },
+      {
+        Header: 'Completed',
+        accessor: 'complete',
+        Cell: ({ row }) => (
+          <input
+            type="checkbox"
+            checked={row.original.complete}
+            onChange={() => toggleTodo(row.original.id)}
+          />
+        ),
+      },
+      {
+        Header: 'Edit',
+        accessor: 'id',
+        Cell: ({ row }) => {
+          if (row.original.id === editingTodoId) {
+            return null; // Hide the edit button while editing
+          } else {
+            return (
+              <button onClick={() => setEditingTodoId(row.original.id)}>
+                Edit
+              </button>
+            );
+          }
+        },
+      },
+    ],
+    [toggleTodo, editingTodoId, editValue, handleEditSave]
+  );
 
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data });
 
-    /*    console.log('5')
-    return (
-        <div>
-            {lists.map(arrEach => (
-                <TodoEach key={arrEach.id} toggleTodo={toggleTodo} editTodo={editTodo} listEach={arrEach}/>
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
             ))}
-        </div>
-    )    */
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
